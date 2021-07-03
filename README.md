5278806c1b836bf26a81eeeec7eee31ccf2fcba0
ucoder92 <42731921+ucoder92@users.noreply.github.com>
2021-07-03 15:12:38 +0300

    Version 1.0.0

--- README.md
+++ README.md
@@ -1,1 +1,124 @@
-# partload-js
+# Page load
+
+Load pages by URL without refreshing the page. Works only for the current domain and subdomains.
+
+This library requires [jQuery](https://jquery.com/download/). Make sure you have it before starting.
+
+### Requirements
+
+- [jQuery](https://jquery.com/download/)
+
+### Before start
+
+After including the `page-load.min.js` file in the project, you need to call `pageLoadInit()`, and everything seems to be ready, but this is not enough. I need a **preloader** to show that the page is loading while **pageLoad** is getting and loading the page. In my case, I used the favorite library [NProgress](https://ricostacruz.com/nprogress/). 
+
+### Init script
+
+After including **NProgress** loader to the project, create a **scripts.js** file and call the `pageLoadInit()` function:
+
+```js
+pageLoadInit({
+    selector: 'a',
+    excludeJS: [
+        'https://code.jquery.com/jquery-3.6.0.min.js',
+        'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js',
+        'owl.carousel.min.js',
+        'page-load.js'
+    ],
+    excludeElement: [
+        '#nprogress',
+    ],
+    beforeSend: function (href, data) {
+        NProgress.start();
+    },
+    success: function (href, data, html) {
+        NProgress.done();
+    },
+    error: function (href, e, data) {
+        console.log('Error: ' + e);
+    }
+});
+```
+
+### Configurations
+
+You can use some useful configurations to customize the library.
+
+```js
+pageLoadInit({
+    // Write configurations here
+});
+```
+
+Below is a complete list of configurations
+
+#### Selector
+
+Select an item to call the function on click:
+
+`selector: 'a'`
+
+*You can give tag name, #myid, .my-class and etc.*
+
+*By default: `'a'`*
+
+--
+
+#### Exclude JS
+
+Every time the page is loaded we don't want to reload any js libraries. This is why we use **excludeJS** to exclude some of them.
+
+`excludeJS: []`
+
+*You can give in array the src of the js library*
+
+*By default: `[]`*
+
+--
+
+#### Exclude element
+
+If you have a div or any tag that you want to keep on page load then use **excludeElement**.
+
+`excludeElement: []`
+
+*You can give in array the tag name, #myid, .my-class and etc.*
+
+*By default: `[]`*
+
+--
+
+#### Event: before send
+
+If you want to do some magic things *before submitting* the request.
+
+```js
+beforeSend: function (href, data) {
+    // your code...
+}
+```
+
+--
+
+#### Event: success
+
+If you want to do some magical things on a *success* request.
+
+```js
+success: function (href, html, data) {
+    // your code...
+}
+```
+
+--
+
+#### Event: error
+
+If you want to do some magical things on a *error* request.
+
+```js
+error: function (href, e, data) {
+    // your code...
+}
+```
+
---
+++ page-load.js
@@ -0,0 +1,363 @@
+/*
+ Page load - jQuery library
+ URL: https://github.com/ucoder92/pageload-js
+ Version: 1.0.0
+ */
+
+var pageLoadInit = function (page_load_config) {
+    var selector = 'a';
+    var excludeJS = [];
+    var excludeElement = ['[page-load-exclude="1"]', '[page-load-exclude="true"]'];
+
+    var html = $('html')[0].outerHTML;
+    var current_url = window.location.href;
+    var site_url = document.location.origin;
+
+    if (page_load_config.selector != undefined && page_load_config.selector != '') {
+        selector = page_load_config.selector;
+    }
+
+    if (page_load_config.excludeJS != undefined && page_load_config.excludeJS.length > 0) {
+        excludeJS = page_load_config.excludeJS;
+    }
+
+    if (page_load_config.excludeElement != undefined && page_load_config.excludeElement.length > 0) {
+        excludeElement = excludeElement.concat(page_load_config.excludeElement);
+    }
+
+    window.history.pushState({ "data": html, "href": current_url }, $('title').text(), current_url);
+
+    $(selector).on('click', function () {
+        var href = $(this).attr('href');
+        var disable = $(this).attr('page-load-disable');
+        var run = false;
+
+        if (disable != undefined && (disable == 'true' || disable == '1' || disable === true)) {
+            return true;
+        }
+
+        if (href != undefined && href != '#' && href != '') {
+            var hash = href.charAt(0);
+            var parsed_url = '';
+
+            if (hash != '#') {
+                var parser = document.createElement('a');
+
+                parser.href = href;
+                parsed_url = parser.href;
+            }
+
+            if (parsed_url != undefined && parsed_url != '') {
+                var url = new URL(parsed_url);
+
+                if (url != undefined && url.origin != undefined && url.origin == site_url) {
+                    run = true;
+                }
+            }
+        }
+
+        if (run) {
+            pageLoadFromURL(href);
+            return false;
+        }
+    });
+
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        var data = $(this).serialize();
+        var action = $(this).attr('action');
+        var method = $(this).attr('method');
+
+        if (data != undefined && data != '') {
+            var url = window.location.href;
+
+            if (action != undefined && action != '') {
+                url = action;
+            }
+
+            if (method != undefined && method != 'POST') {
+                if (url.indexOf("?") > -1) {
+                    url = url.substr(0, url.indexOf("?"));
+                }
+
+                url = url + '?' + data;
+            }
+
+            pageLoadFromURL(url, data);
+        }
+    });
+
+    window.onpopstate = function (e) {
+        if (e.state) {
+            var data = e.state.data;
+            pageLoad(data);
+        }
+    };
+
+    function pageLoadFromURL(href, data) {
+        window.stop();
+        var page_data = {};
+
+        if (data != undefined) {
+            page_data = data;
+        }
+
+        $.ajax({
+            url: href,
+            type: 'GET',
+            data: page_data,
+            beforeSend: function () {
+                if (page_load_config.beforeSend != undefined) {
+                    page_load_config.beforeSend(href, page_data);
+                }
+            },
+            success: function (html) {
+                if (page_load_config.success != undefined) {
+                    page_load_config.success(href, page_data, html);
+                }
+
+                if (html != undefined && html != '') {
+                    pageLoad(html, href);
+                }
+            },
+            error: function (e) {
+                if (page_load_config.error != undefined) {
+                    page_load_config.error(href, e, page_data);
+                }
+
+                pageLoad(e.responseText, href);
+            }
+        });
+    }
+
+    function pageLoad(data, href) {
+        var $doc;
+
+        if (data != undefined && data != '') {
+            var parser = new DOMParser();
+            var docs = parser.parseFromString(data, 'text/html');
+            var $doc = $(docs);
+        }
+
+        if ($doc != undefined && $doc.length > 0) {
+            var $html = $doc.children('html');
+            var $head = $html.children('head');
+            var $body = $html.children('body');
+
+            var head_objects = [];
+            var exclude_objects = [];
+
+            var body_outerHTML = '';
+            var head_html = $head.html();
+            var head_before_outerHTML = '';
+
+            $('head').children().each(function () {
+                var rm = true;
+                var $this = $(this);
+                var outerHTML = $this.prop('outerHTML');
+
+                if (outerHTML != undefined && outerHTML != '') {
+                    var disable = $this.attr('page-load-disable');
+                    var index = head_html.indexOf(outerHTML);
+
+                    if (disable != undefined && (disable == 'true' || disable == '1' || disable === true) && index > -1) {
+                        rm = false;
+                    } else if ($this.is('script')) {
+                        var src = $this.attr('src');
+
+                        if (src != undefined && src != '' && index > -1) {
+                            rm = false;
+                        }
+                    } else if ($this.is('link')) {
+                        var href = $this.attr('href');
+
+                        if (href != undefined && href != '' && index > -1) {
+                            rm = false;
+                        }
+                    } else if (index > -1) {
+                        rm = false;
+                    }
+                }
+
+                if (rm) {
+                    $this.remove();
+                } else {
+                    head_objects.push({
+                        html: outerHTML,
+                        element: $this,
+                    });
+                }
+            });
+
+            $head.children().each(function () {
+                var $this = $(this);
+                var outerHTML = $this.prop('outerHTML');
+                var index = head_objects.findIndex(x => x.html === outerHTML);
+
+                if (index < 0 && head_before_outerHTML != undefined && head_before_outerHTML != '') {
+                    var beforeIndex = head_objects.findIndex(x => x.html === head_before_outerHTML);
+
+                    if (beforeIndex > -1) {
+                        var object = head_objects[beforeIndex];
+
+                        if (object != undefined && object.element != undefined) {
+                            $(outerHTML).insertAfter(object.element);
+
+                            head_objects.push({
+                                html: outerHTML,
+                                element: object.element.next(),
+                            });
+                        }
+                    }
+                }
+
+                head_before_outerHTML = outerHTML;
+            });
+
+            if (excludeElement != undefined && excludeElement.length > 0) {
+                $.each(excludeElement, function (i, value) {
+                    var elements = $('body').children(value);
+
+                    if (elements != undefined && elements.length > 0) {
+                        elements.each(function () {
+                            var outerHTML = $(this).prop('outerHTML');
+
+                            exclude_objects.push({
+                                tag: value,
+                                html: outerHTML,
+                                element: $(this),
+                            });
+                        });
+                    }
+                });
+            }
+
+            $body.children().each(function () {
+                var set = true;
+                var $this = $(this);
+                var outerHTML = $this.prop('outerHTML');
+
+                if (outerHTML != undefined && outerHTML != '') {
+                    var attrs = [];
+                    var tagName = $this.prop('tagName').toLowerCase();
+
+                    if ($this.is('script')) {
+                        var src = $this.attr('src');
+                        var disable = $this.attr('page-load-exclude-js');
+
+                        if (disable != undefined && (disable === true || disable == 'true' || disable == '1') || excludeJS.indexOf(src) != -1) {
+                            set = false;
+                        }
+                    }
+
+                    if (exclude_objects != undefined && exclude_objects.length > 0) {
+                        var index = exclude_objects.findIndex(x => x.tag === tagName);
+
+                        if (index > -1) {
+                            exclude_objects.splice(index, 1);
+                        } else {
+                            var attrs = [];
+
+                            $.each(this.attributes, function () {
+                                if (this.specified) {
+                                    var name = this.name;
+                                    var value = this.value;
+
+                                    switch (name) {
+                                        case 'id':
+                                            if (value != '') {
+                                                attrs.push('#' + value);
+                                            }
+                                            break;
+                                        case 'class':
+                                            if (value != '') {
+                                                var list = value.split(" ");
+
+                                                $.each(list, function (i, v) {
+                                                    attrs.push('.' + v);
+                                                });
+                                            }
+                                            break;
+                                        default:
+                                            attrs.push('[' + name + ']');
+
+                                            if (value != '') {
+                                                attrs.push('[' + name + '="' + value + '"]');
+                                                attrs.push("[" + name + "='" + value + "']");
+                                            }
+
+                                            break;
+                                    }
+                                }
+                            });
+
+                            if (attrs.length > 0) {
+                                $.each(attrs, function (i, value) {
+                                    var index = exclude_objects.findIndex(x => x.tag === value);
+
+                                    if (index > -1) {
+                                        exclude_objects.splice(index, 1);
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+
+                if (set) {
+                    body_outerHTML += outerHTML;
+                }
+            });
+
+            if (exclude_objects != undefined && exclude_objects.length > 0) {
+                $.each(exclude_objects, function (i, item) {
+                    var html = item.html;
+
+                    if (html != undefined && html != '') {
+                        body_outerHTML += html;
+                    }
+                });
+            }
+
+            // Set to html
+            $('body').html(body_outerHTML);
+
+            // Remove attrs
+            while ($('html')[0].attributes.length > 0) {
+                $('html')[0].removeAttributeNode($('html')[0].attributes[0]);
+            }
+
+            if ($html[0].attributes.length > 0) {
+                $.each($html[0].attributes, function (i, value) {
+                    if (this.specified) {
+                        var name = this.name;
+                        var value = this.value;
+
+                        $('html').attr(name, value);
+                    }
+                });
+            }
+
+            while ($('body')[0].attributes.length > 0) {
+                $('body')[0].removeAttributeNode($('body')[0].attributes[0]);
+            }
+
+            if ($body[0].attributes.length > 0) {
+                $.each($body[0].attributes, function (i, value) {
+                    if (this.specified) {
+                        var name = this.name;
+                        var value = this.value;
+
+                        $('body').attr(name, value);
+                    }
+                });
+            }
+
+            // Set document
+            document = parser.parseFromString(data, 'text/html');
+            window.history.replaceState({ "data": data, "href": href }, $('title').text(), href);
+        } else {
+            window.location.reload();
+        }
+    }
+};
